fa47dd3fda753a226d7d89cc4b3366d45f1dc0db
aAdityaapillai <aadityaa.pillai@gmail.com>
2021-12-27 17:50:58 +0400

    change text

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>"For those about to shop, I salute you! ~Luna Loud"</Text>
+      <Text>“Well done, Frank Zhang,” Leo said dryly, doing his impression of Chiron the centaur. “That is exactly how people beat Chinese handcuffs. They turn into iguanas.”</Text>
       <StatusBar style="auto" />
     </View>
   );
